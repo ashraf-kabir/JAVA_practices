aa94923230a1410aefc60989858dac48d4b9a1f3
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-20 22:18:07 +0600

    changes

--- Dec14T9.java
+++ Dec14T9.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
 public class Dec14T9{
-  public static void main(String args[]){
-    
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Enter the first number: ");
-    int x=sc.nextInt();
-    
-    System.out.println("Enter the second number: ");
-    int y=sc.nextInt();
-    
-    System.out.println("Enter the third number: ");
-    int z=sc.nextInt();
-    
-    if (x>y && x>z){
-      System.out.println(x + " is the largest number");
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the first number: ");
+        int x=sc.nextInt();
+        
+        System.out.println("Enter the second number: ");
+        int y=sc.nextInt();
+        
+        System.out.println("Enter the third number: ");
+        int z=sc.nextInt();
+        
+        if (x>y && x>z){
+            System.out.println(x + " is the largest number");
+        }
+        
+        else if (y>x && y>z){
+            System.out.println(y + " is the largest number");
+        }
+        
+        else {
+            System.out.println(z + " is the largest number");
+        }
     }
-    
-    else if (y>x && y>z){
-      System.out.println(y + " is the largest number");
-    }
-    
-    else {
-      System.out.println(z + " is the largest number");
-    }
-  }
 }
