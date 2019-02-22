61cce50d5443ce3d9a4a632932e65886bfc15cba
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-22 12:10:07 +0600

    changes

--- Three56.java
+++ Three56.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 public class Three56 {
-  public static void main (String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Enter your birth year:");
-    int birthYear = sc.nextInt();
-    
-    System.out.println("Enter your birth month:");
-    int birthMonth = sc.nextInt();
-    
-    int countY=0;
-    for (; birthYear<=2016; birthYear++) {
-      countY++;
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter your birth year:");
+        int birthYear = sc.nextInt();
+        
+        System.out.println("Enter your birth month:");
+        int birthMonth = sc.nextInt();
+        
+        int countY=0;
+        for (; birthYear<=2016; birthYear++) {
+            countY++;
+        }
+        System.out.print(countY + " years");
+        System.out.println();
     }
-    System.out.print(countY + " years");
-    System.out.println();
-  }
 }
