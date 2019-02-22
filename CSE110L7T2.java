9ad813b78cb534a710a05695468f0518a4c8b59f
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-22 12:31:28 +0600

    changes

--- CSE110L7T2.java
+++ CSE110L7T2.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class CSE110L7T2{
-  public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    
-    int a=sc.nextInt();
-    
-    for(int c=1; c<=a; c++){
-      System.out.print("*");
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        int a=sc.nextInt();
+        
+        for(int c=1; c<=a; c++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
-    System.out.println();
-  }
 }
--- CSE110L7T3.java
+++ CSE110L7T3.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class CSE110L7T3{
-  public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    
-    int row=sc.nextInt();
-    int col=sc.nextInt();
-    
-    for(int rowC=1; rowC<=row; rowC++){
-      for(int colC=1; colC<=col; colC++){
-        System.out.print("*");
-      }
-      System.out.println();
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        
+        for(int rowC=1; rowC<=row; rowC++){
+            for(int colC=1; colC<=col; colC++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
-  }
 }
