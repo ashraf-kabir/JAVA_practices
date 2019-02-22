9b17d0543ee8780395bd751abcfea42a44c1ede7
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-22 12:32:02 +0600

    changes

--- Test1143.java
+++ Test1143.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class Test1143{
-  public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    
-    int a=sc.nextInt();
-    
-    for(int b=1; b<=a; b++){
-      int j=b*b;
-      int k=b*b*b;
-      System.out.format("%d %d %d\n",b,j,k);
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+        int a=sc.nextInt();
+        
+        for(int b=1; b<=a; b++){
+            int j=b*b;
+            int k=b*b*b;
+            System.out.format("%d %d %d\n",b,j,k);
+        }
     }
-  }
 }
