758d3ecda8532af478c267622292a33eade5008f
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-23 14:03:15 +0600

    Update TestSeries1.java

--- TestSeries1.java
+++ TestSeries1.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class TestSeries1{
-  public static void main(String[] args){
-    
-    Scanner sc=new Scanner(System.in);
-    
-    System.out.println("Enter number: ");
-    int num=sc.nextInt();
-    
-    int sum=0;
-    
-    for(int a=1; a<=num; a++){
-      for(int b=1; b<=a; b++){
-        sum+=Math.pow(b,2);
-      }
+    public static void main(String[] args){
+        
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter number: ");
+        int num=sc.nextInt();
+        
+        int sum=0;
+        
+        for(int a=1; a<=num; a++){
+            for(int b=1; b<=a; b++){
+                sum+=Math.pow(b,2);
+            }
+        }
+        System.out.println(sum);
     }
-    System.out.println(sum);
-  }
 }
