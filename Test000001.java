a6a998e2fe6d0f473d93d5c50ee33db04cdabdc4
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-23 14:26:00 +0600

    Update Test000001.java

--- Test000001.java
+++ Test000001.java
@@ -1,6 +1,6 @@
 public class Test000001{
-  public static void main(String args[]){
-    
-    System.out.format("No%n");
-  }
+    public static void main(String args[]){
+        
+        System.out.format("No%n");
+    }
 }
