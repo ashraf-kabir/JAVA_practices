183b52d9823e0e77bb60e7ca0085d4556e4f2d27
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-24 10:11:32 +0600

    changes

--- 20170410002/Test.java
+++ 20170410002/Test.java
@@ -1,9 +1,9 @@
 public class Test{
-  public static void main(String[] args){
-    
-    FinalT6A q1 = new FinalT6A(5,6);
-    
-    q1.methodA();
-    q1.methodA();
-  }
+    public static void main(String[] args){
+        
+        FinalT6A q1 = new FinalT6A(5,6);
+        
+        q1.methodA();
+        q1.methodA();
+    }
 }
