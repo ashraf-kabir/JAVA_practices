652b880bf7c96d8da735147cc8af23454b461572
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-24 10:15:03 +0600

    changes

--- Increment.java
+++ Increment.java
@@ -1,14 +1,14 @@
 public class Increment{
-  public static void main(String[] args){
-    
-    int a=5;
-    int b=6;
-    int d=8;
-    int e=9;
-    int 
-    System.out.println(++a);
-    System.out.println(b++);
-    System.out.println(--d);
-    System.out.println(e--);
-  }
+    public static void main(String[] args){
+        
+        int a=5;
+        int b=6;
+        int d=8;
+        int e=9;
+        int 
+            System.out.println(++a);
+        System.out.println(b++);
+        System.out.println(--d);
+        System.out.println(e--);
+    }
 }
