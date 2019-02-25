cd631c07c6b7821a14fecbb84b44eb160ada9265
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-25 20:42:35 +0600

    changes

--- 20170410001/A.java
+++ 20170410001/A.java
@@ -1,17 +1,17 @@
 class A{
-  public static int temp = 4;
-  public int sum;
-  public int y;
-  public A(int x){
-    y = temp - 2 + x;
-    sum = temp + 3;
-    temp-=2;
-  }
-  public void methodB(int m, int n){
-    int x = 0;
-    y = y + m + (temp++);
-    x = x + 2 +  n;
-    sum = sum + x + y;
-    System.out.println(x + " " + y+ " " + sum);  
-  }
+    public static int temp = 4;
+    public int sum;
+    public int y;
+    public A(int x){
+        y = temp - 2 + x;
+        sum = temp + 3;
+        temp-=2;
+    }
+    public void methodB(int m, int n){
+        int x = 0;
+        y = y + m + (temp++);
+        x = x + 2 +  n;
+        sum = sum + x + y;
+        System.out.println(x + " " + y+ " " + sum);  
+    }
 }
