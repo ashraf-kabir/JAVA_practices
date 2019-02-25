ff992bab686ac7a5a4d1ba138fb5f89ad2f9a2e2
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-02-25 20:42:02 +0600

    changes

--- 20170409003/Test.java
+++ 20170409003/Test.java
@@ -1,15 +1,15 @@
 public class Test{
-  public static void main(String[] args){
-    
-    
-    Foo foo1 = new Bar();
-    Foo foo2 = new Baz();
-    Bar bar1 = new Bar();
-    Baz baz1 = new Baz();
-    Baz baz2 = new Mumble();
-    Object obj1 = new Bar();
-    
-    //((Baz)obj1).method2();
+    public static void main(String[] args){
+        
+        
+        Foo foo1 = new Bar();
+        Foo foo2 = new Baz();
+        Bar bar1 = new Bar();
+        Baz baz1 = new Baz();
+        Baz baz2 = new Mumble();
+        Object obj1 = new Bar();
+        
+        //((Baz)obj1).method2();
 //obj1.method1();
 //((Mumble)baz1).method3();
 //foo1.method2();
@@ -28,8 +28,8 @@ public class Test{
 //baz1.method1();
 //((Baz)foo2).method3();
 //baz2.method1();
-((Bar)foo1).method3();
-
-
-  }
+        ((Bar)foo1).method3();
+        
+        
+    }
 }
