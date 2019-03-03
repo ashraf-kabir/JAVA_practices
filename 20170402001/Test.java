e14e3652845c11e0dab767b4abdcaa4cd8bb1b2c
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-03 23:46:36 +0600

    changes

--- 20170402001/Test.java
+++ 20170402001/Test.java
@@ -1,16 +1,16 @@
 public class Test {
-
- public static void main(String[] args) {
-  
-  SavingsAccount saver1 = new SavingsAccount(20000.00);
-  SavingsAccount saver2 = new SavingsAccount(30000.00);
-  
-  saver1.calculateMonthlyInterest();
+    
+    public static void main(String[] args) {
+        
+        SavingsAccount saver1 = new SavingsAccount(20000.00);
+        SavingsAccount saver2 = new SavingsAccount(30000.00);
+        
+        saver1.calculateMonthlyInterest();
         saver1.printSavingsBalance( );
         
         saver2.modifyInterestRate(5.5);
         saver2.calculateMonthlyInterest();
         saver2.printSavingsBalance();
- }
-
+    }
+    
 }
