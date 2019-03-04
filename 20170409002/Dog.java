7aecc0f1c45f9a4b42c8e8f1a14e3cc69c9ba608
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-04 23:15:51 +0600

    changes

--- 20170409002/Dog.java
+++ 20170409002/Dog.java
@@ -1,25 +1,25 @@
-public class Dog{
-  
-  private String color = "Black";
-  private String action = "";
-  public Dog(){
-  }
-  public Dog(String name){
-    color = name;
-  }
-  public void setColor(String x){
-    color = x;
-  }
-  public String getColor(){
-    return color;
-  }
-  public void setAction(String y){
-    action = y;
-  }
-  public String getAction(){
-    return action;
-  }
-  public void printDog(){
-    System.out.println(getColor()+" is "+getAction());
-  }
+public class Dog {
+    
+    private String color = "Black";
+    private String action = "";
+    public Dog() {
+    }
+    public Dog(String name) {
+        color = name;
+    }
+    public void setColor(String x) {
+        color = x;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setAction(String y) {
+        action = y;
+    }
+    public String getAction() {
+        return action;
+    }
+    public void printDog() {
+        System.out.println(getColor()+" is "+getAction());
+    }
 }
