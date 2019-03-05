839088cfc9e3041d7a43779a3c2c48a9bfa57519
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-05 23:53:06 +0600

    changes

--- Task18b.java
+++ Task18b.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Task18b{
-  public static void main(String [] args){
-    
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Enter number");
-    int n = sc.nextInt();
-    for(int c=0;c<=n ;c++){
-      if (n!=0){
-        n/=10;
-      }
-      System.out.println(n);
+    public static void main(String [] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        for(int c=0;c<=n ;c++){
+            if (n!=0){
+                n/=10;
+            }
+            System.out.println(n);
+        }
+        
+        
     }
     
-    
-  }
-
 }
