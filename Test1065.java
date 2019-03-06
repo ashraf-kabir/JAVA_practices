b4331757f9be2c633b27492aa4a495fc00fa6a06
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-06 18:50:54 +0600

    changes

--- Test1065.java
+++ Test1065.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Test1065{
-  public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    
-    int num[]= new int[5];
-    int d=0;
-    
-    for(int a=0; a<num.length; a++){
-      num[a]=sc.nextInt();
-      if(num[a]%2==0){
-        d++;
-      }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        int num[]= new int[5];
+        int d=0;
+        
+        for(int a=0; a<num.length; a++){
+            num[a]=sc.nextInt();
+            if(num[a]%2==0){
+                d++;
+            }
+        }
+        System.out.println(d+" valores pares");
     }
-    System.out.println(d+" valores pares");
-  }
 }
