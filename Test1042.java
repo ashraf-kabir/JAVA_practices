3316053ca64a43aa0c6ffcc3c6a68637bce0ff04
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-12 22:56:25 +0600

    changes

--- Test1042.java
+++ Test1042.java
@@ -1,39 +1,39 @@
 import java.util.Scanner;
 
 public class Test1042{
-  public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    
-    int a=sc.nextInt();
-    int b=sc.nextInt();
-    int c=sc.nextInt();
-    
-    if(a<b && a<c){
-      if(b<c){
-        System.out.printf("%d\n%d\n%d\n",a,b,c);
-      }
-      else {
-        System.out.printf("%d\n%d\n%d\n",a,c,b);
-      }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        
+        if(a<b && a<c){
+            if(b<c){
+                System.out.printf("%d\n%d\n%d\n",a,b,c);
+            }
+            else {
+                System.out.printf("%d\n%d\n%d\n",a,c,b);
+            }
+        }
+        
+        if(b<a && b<c){
+            if(a<c){
+                System.out.printf("%d\n%d\n%d\n",b,a,c);
+            }
+            else {
+                System.out.printf("%d\n%d\n%d\n",b,c,a);
+            }
+        }
+        
+        if(c<b && c<a){
+            if(b<a){
+                System.out.printf("%d\n%d\n%d\n",c,b,a);
+            }
+            else {
+                System.out.printf("%d\n%d\n%d\n",c,a,b);
+            }
+        }
+        System.out.printf("\n%d\n%d\n%d\n",a,b,c);
     }
-    
-    if(b<a && b<c){
-      if(a<c){
-        System.out.printf("%d\n%d\n%d\n",b,a,c);
-      }
-      else {
-        System.out.printf("%d\n%d\n%d\n",b,c,a);
-      }
-    }
-    
-    if(c<b && c<a){
-      if(b<a){
-        System.out.printf("%d\n%d\n%d\n",c,b,a);
-      }
-      else {
-        System.out.printf("%d\n%d\n%d\n",c,a,b);
-      }
-    }
-    System.out.printf("\n%d\n%d\n%d\n",a,b,c);
-  }
 }
