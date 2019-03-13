bb643c90e77b1fe65da767a3e68ebad2c70ca706
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-13 19:53:14 +0600

    changes

--- Task9test.java
+++ Task9test.java
@@ -1,19 +1,19 @@
 public class Task9test {
- public static void main(String [] args){
-  int sum = 0;
-  
-  for( int i= 1; i<= 65; i++){
-    
-   if(i%7==0){
-    if(i%9!=0){
-     sum+=i;
+    public static void main(String [] args){
+        int sum = 0;
+        
+        for( int i= 1; i<= 65; i++){
+            
+            if(i%7==0){
+                if(i%9!=0){
+                    sum+=i;
+                }
+            }
+            else if(i%9==0){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
     }
-   }
-   else if(i%9==0){
-    sum+=i;
-   }
-  }
-  System.out.println(sum);
- }
-
+    
 }
