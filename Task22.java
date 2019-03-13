3fc52522afb4fee5683889eeaba5c5c23a8a6e31
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-13 19:53:32 +0600

    changes

--- Task22.java
+++ Task22.java
@@ -1,45 +1,45 @@
 import java.util.Scanner;
 
 public class Task22{
-  public static void main(String[] args){
-    
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Enter number:");
-    int a = sc.nextInt();
-    
-    if (a==0){
-      System.out.println("Zero");
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter number:");
+        int a = sc.nextInt();
+        
+        if (a==0){
+            System.out.println("Zero");
+        }
+        else if (a==1){
+            System.out.println("One");
+        }
+        else if (a==2){
+            System.out.println("Two");
+        }
+        else if (a==3){
+            System.out.println("Three");
+        }
+        else if (a==4){
+            System.out.println("Four");
+        }
+        else if (a==5){
+            System.out.println("Five");
+        }
+        else if (a==6){
+            System.out.println("Six");
+        }
+        else if (a==7){
+            System.out.println("Seven");
+        }
+        else if (a==8){
+            System.out.println("Eight");
+        }
+        else if (a==9){
+            System.out.println("Nine");
+        }
+        else{
+            System.out.println("Wrong input");
+        }
     }
-    else if (a==1){
-      System.out.println("One");
-    }
-    else if (a==2){
-      System.out.println("Two");
-    }
-    else if (a==3){
-      System.out.println("Three");
-    }
-    else if (a==4){
-      System.out.println("Four");
-    }
-    else if (a==5){
-      System.out.println("Five");
-    }
-    else if (a==6){
-      System.out.println("Six");
-    }
-    else if (a==7){
-      System.out.println("Seven");
-    }
-    else if (a==8){
-      System.out.println("Eight");
-    }
-    else if (a==9){
-      System.out.println("Nine");
-    }
-    else{
-      System.out.println("Wrong input");
-    }
-  }
 }
