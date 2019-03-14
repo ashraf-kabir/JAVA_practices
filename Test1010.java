b63f070554a5cb72250de3348295b561493d0431
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:18:32 +0600

    changes

--- Test1010.java
+++ Test1010.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Test1010{
-  public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    
-    int serialA=sc.nextInt();
-    int qA=sc.nextInt();
-    double aA=sc.nextDouble();
-    
-    
-    int serialB=sc.nextInt();
-    int qB=sc.nextInt();
-    double aB=sc.nextDouble();
-    
-    double pay =(double)qA*aA+(double)qB*aB;
-    
-    System.out.print("VALOR A PAGAR: R$ ");
-    System.out.printf("%.2f", pay);
-    System.out.println();
-  }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        int serialA=sc.nextInt();
+        int qA=sc.nextInt();
+        double aA=sc.nextDouble();
+        
+        
+        int serialB=sc.nextInt();
+        int qB=sc.nextInt();
+        double aB=sc.nextDouble();
+        
+        double pay =(double)qA*aA+(double)qB*aB;
+        
+        System.out.print("VALOR A PAGAR: R$ ");
+        System.out.printf("%.2f", pay);
+        System.out.println();
+    }
 }
