47ac435e12f9d00c6fce87e276aad66a8ae51489
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:12:17 +0600

    changes

--- Test1154.java
+++ Test1154.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Test1154{
-  public static void main(String[] args){
-    
-    Scanner sc = new Scanner(System.in);
-    
-  
-  }
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+    }
 }
