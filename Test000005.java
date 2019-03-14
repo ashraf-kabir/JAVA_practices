54fc932bd095032665db9cce283775778de77a14
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:20:41 +0600

    changes

--- Test000005.java
+++ Test000005.java
@@ -1,11 +1,11 @@
 public class Test000005{
-
-  public static void main(String[] args) {
-    String string = "bla*h";
-    StringBuilder sb = new StringBuilder(string);
-    sb.deleteCharAt(4);
-    // Prints out "blah"
-    System.out.println(sb.toString());
-  }
-
+    
+    public static void main(String[] args) {
+        String string = "bla*h";
+        StringBuilder sb = new StringBuilder(string);
+        sb.deleteCharAt(4);
+        // Prints out "blah"
+        System.out.println(sb.toString());
+    }
+    
 }
--- Test00002.java
+++ Test00002.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Test00002{
-  public static void main(String args[]){
-    
-    Scanner sc=new Scanner(System.in);
-    
-    double R=sc.nextDouble();
-    
-    double A= 3.14159*(R*R);
-    
-    System.out.print("A=");
-    System.out.printf("%.4f", A);
-    
-    System.out.println();
-  }
+    public static void main(String args[]){
+        
+        Scanner sc=new Scanner(System.in);
+        
+        double R=sc.nextDouble();
+        
+        double A= 3.14159*(R*R);
+        
+        System.out.print("A=");
+        System.out.printf("%.4f", A);
+        
+        System.out.println();
+    }
 }
