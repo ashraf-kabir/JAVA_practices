4d946258293d73c2782f087f6a4cdcfd0b4d48a3
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:17:44 +0600

    changes

--- Test1006.java
+++ Test1006.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Test1006{
-  public static void main(String []args){
-    Scanner sc=new Scanner(System.in);
-    
-    double A=sc.nextDouble();
-    double B=sc.nextDouble();
-    double C=sc.nextDouble();
-    
-    double average=((A*2+B*3+C*5))/10.0;
-    
-    System.out.print("MEDIA = ");
-    System.out.printf("%.1f", average);
-    System.out.println();
-  }
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        
+        double A=sc.nextDouble();
+        double B=sc.nextDouble();
+        double C=sc.nextDouble();
+        
+        double average=((A*2+B*3+C*5))/10.0;
+        
+        System.out.print("MEDIA = ");
+        System.out.printf("%.1f", average);
+        System.out.println();
+    }
 }
--- Test1008.java
+++ Test1008.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Test1008{
-  public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-    
-    int a=sc.nextInt();
-    int b=sc.nextInt();
-    double c=sc.nextDouble();
-    
-    double x=b*c;
-    
-    System.out.println("NUMBER = "+a);
-    System.out.print("SALARY = U$ ");
-    System.out.printf("%.2f", x);
-    System.out.println();
-  }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double c=sc.nextDouble();
+        
+        double x=b*c;
+        
+        System.out.println("NUMBER = "+a);
+        System.out.print("SALARY = U$ ");
+        System.out.printf("%.2f", x);
+        System.out.println();
+    }
 }
