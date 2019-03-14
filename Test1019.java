d2004c775887320a17d135f0ceabbea9ea377309
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:14:46 +0600

    changes

--- Test1019.java
+++ Test1019.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Test1019{
-  public static void main(String args[]){
-    
-    Scanner sc=new Scanner(System.in);
-    
-    int N=sc.nextInt();
-    
-    int hrs=N/3600;
-    int remN=N%3600;
-    int mins=remN/60;
-    int seconds=remN%60;
-    
-    System.out.println(hrs+":"+mins+":"+seconds);
-  }
+    public static void main(String args[]){
+        
+        Scanner sc=new Scanner(System.in);
+        
+        int N=sc.nextInt();
+        
+        int hrs=N/3600;
+        int remN=N%3600;
+        int mins=remN/60;
+        int seconds=remN%60;
+        
+        System.out.println(hrs+":"+mins+":"+seconds);
+    }
 }
