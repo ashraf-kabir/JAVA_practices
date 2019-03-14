571fc36897b73ea239b705ee1730d9054a09c1f1
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:19:49 +0600

    changes

--- Test1013.java
+++ Test1013.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Test1013{
-  
-  public static void main(String argsg[]){
     
-    Scanner sc=new Scanner(System.in);
-    
-    int a=sc.nextInt();
-    int b=sc.nextInt();
-    int c=sc.nextInt();
-    
-    int maiorab = ((a+b + Math.abs(a-b))/2);
-    int maiorabc = ((maiorab+c+ Math.abs(maiorab-c))/2);
-    
-    System.out.println(maiorabc+" eh o maior");
-  }
+    public static void main(String argsg[]){
+        
+        Scanner sc=new Scanner(System.in);
+        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        
+        int maiorab = ((a+b + Math.abs(a-b))/2);
+        int maiorabc = ((maiorab+c+ Math.abs(maiorab-c))/2);
+        
+        System.out.println(maiorabc+" eh o maior");
+    }
 }
