ad8584520f8a5669838992aa137c78d1c76e46a2
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:18:50 +0600

    changes

--- Test1011.java
+++ Test1011.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Test1011{
-  public static void main(String args []){
-    
-    Scanner sc=new Scanner(System.in);
-    
-    double R=sc.nextDouble();
-    
-    double VOLUME= (4.0/3)*3.14159*(R*R*R);
-    
-    System.out.print("VOLUME = ");
-    System.out.printf("%.3f", VOLUME);
-    System.out.println();
-  }
+    public static void main(String args []){
+        
+        Scanner sc=new Scanner(System.in);
+        
+        double R=sc.nextDouble();
+        
+        double VOLUME= (4.0/3)*3.14159*(R*R*R);
+        
+        System.out.print("VOLUME = ");
+        System.out.printf("%.3f", VOLUME);
+        System.out.println();
+    }
 }
