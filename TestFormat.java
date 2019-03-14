f2986fcf13df2921efaeb1c5e0bdf713cbba5ad4
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:12:33 +0600

    changes

--- TestFormat.java
+++ TestFormat.java
@@ -4,11 +4,11 @@ import java.util.Locale;
 public class TestFormat {
     
     public static void main(String[] args) {
-      
-      Calendar c = Calendar.getInstance();
-      
-      System.out.format("%tB %te, %tY%n", c, c, c);
-      
-      System.out.format("%tl:%tM %tp%n", c, c, c);
+        
+        Calendar c = Calendar.getInstance();
+        
+        System.out.format("%tB %te, %tY%n", c, c, c);
+        
+        System.out.format("%tl:%tM %tp%n", c, c, c);
     }
 }
