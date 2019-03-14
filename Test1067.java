279ac7f60ba47851f8acb6ecdb63a218b740b9bd
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:10:28 +0600

    changes

--- Test1067.java
+++ Test1067.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Test1067{
-  public static void main(String[] args) {
-    
-    Scanner input =new Scanner(System.in);
-    
-    int X = input.nextInt();
-    System.out.print(1+"\n");
-    
-    for(int a=1; a<X-1; a+=2){
-      int odd = a+2;
-      System.out.print(odd+"\n");
+    public static void main(String[] args) {
+        
+        Scanner input =new Scanner(System.in);
+        
+        int X = input.nextInt();
+        System.out.print(1+"\n");
+        
+        for(int a=1; a<X-1; a+=2){
+            int odd = a+2;
+            System.out.print(odd+"\n");
+        }
     }
-  }
 }
