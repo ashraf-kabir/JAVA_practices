b7174a895022c334a1e3b8734d3b3f6215bee85f
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:09:43 +0600

    changes

--- Test1046.java
+++ Test1046.java
@@ -1,25 +1,25 @@
 import java.util.*;
 
 public class Test1046{
-  
-  public static void main(String[] args){
     
-    Scanner sc = new Scanner(System.in);
-    
-    int x=sc.nextInt();
-    int y=sc.nextInt();
-    
-    int time=0;
-    if((x>=0&&x<=23) && (y>=0&&y<23)){
-      if(x>=y){
-        time = (24-x)+y;
-      }else if(x<y){
-        time = (24-x)-(24-y);
-      }
-      System.out.println("O JOGO DUROU "+time+" HORA(S)");
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        
+        int time=0;
+        if((x>=0&&x<=23) && (y>=0&&y<23)){
+            if(x>=y){
+                time = (24-x)+y;
+            }else if(x<y){
+                time = (24-x)-(24-y);
+            }
+            System.out.println("O JOGO DUROU "+time+" HORA(S)");
+        }
+        else if(x==y){
+            System.out.println("O JOGO DUROU 24 HORA(S)");
+        }
     }
-    else if(x==y){
-      System.out.println("O JOGO DUROU 24 HORA(S)");
-    }
-  }
 }
