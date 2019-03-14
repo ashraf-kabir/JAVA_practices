47a9da70eb62c44e3b65bfea92a96a5242928967
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-14 09:11:25 +0600

    changes

--- Test1075.java
+++ Test1075.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Test1075{
-  public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    
-    int a=sc.nextInt();
-    
-    for(int c=1; c<1000; c++){
-      if(c%a==0){
-        System.out.println(c);
-      }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+        int a=sc.nextInt();
+        
+        for(int c=1; c<1000; c++){
+            if(c%a==0){
+                System.out.println(c);
+            }
+        }
+        
     }
-    
-  }
 }
