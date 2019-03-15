994789b4d19f02f0950e0bed74170520ef81b817
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-15 14:26:12 +0600

    changes

--- CP4Q4.java
+++ CP4Q4.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class CP4Q4{
-  public static void main(String[] args){
-    
-    Scanner sc= new Scanner(System.in);
-    System.out.println("Enter number:");
-    int n= sc.nextInt();
-    
-    int sum=0;
-    
-    for(int c=1; c<=n; c++){
-      sum=sum+c/2*c-1;
+    public static void main(String[] args){
+        
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n= sc.nextInt();
+        
+        int sum=0;
+        
+        for(int c=1; c<=n; c++){
+            sum=sum+c/2*c-1;
+        }
+        System.out.println(sum);
     }
-    System.out.println(sum);
-  }
 }
