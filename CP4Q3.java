f6ba6e278b6d528facaf583f4e8b3e48d24fe630
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-15 14:26:33 +0600

    Update CP4Q3.java

--- CP4Q3.java
+++ CP4Q3.java
@@ -1,12 +1,12 @@
 public class CP4Q3{
-  public static void main(String[] args){
-    
-    int pro=1;
-    for (int c=3; c<=13; c++){
-      if (c%2!=0){
-        pro=pro*c;
-      }
+    public static void main(String[] args){
+        
+        int pro=1;
+        for (int c=3; c<=13; c++){
+            if (c%2!=0){
+                pro=pro*c;
+            }
+        }
+        System.out.println(pro);
     }
-    System.out.println(pro);
-  }
 }
