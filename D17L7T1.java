1140986360e154a2fa6152089888113278cdf5f5
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-03-18 19:54:09 +0600

    changes

--- D17L7T1.java
+++ D17L7T1.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class D17L7T1{
-  public static void main(String args[]){
-    
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Enter range in number: ");
-    int r=sc.nextInt();
-    
-    for(int c=1; c<=r; c++){
-      System.out.print(c);
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter range in number: ");
+        int r=sc.nextInt();
+        
+        for(int c=1; c<=r; c++){
+            System.out.print(c);
+        }
+        System.out.println();
     }
-    System.out.println();
-  }
 }
